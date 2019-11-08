4e36b0b8184c13923e56e885d22862b4207ba6e6
ytalojhonata <you@example.com>
2019-11-07 22:24:35 -0300

    Adicionando as models

---
+++ app/Produto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    protected $fillable = [
+        'nome',
+        'valor',
+        'ativo'
+    ];
+}
