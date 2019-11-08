6d1f3f8c0267ccce481fcc1650e050d70c3f07c3
ytalojhonata <you@example.com>
2019-11-07 22:21:56 -0300

    Adição das migrations

---
+++ database/migrations/2019_11_07_235838_create_produtos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->decimal('valor', 6, 2)->default(0);
+            $table->enum('ativo', ['S', 'N'])->default('S');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
---
+++ database/migrations/2019_11_08_004549_create_pedido_produtos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidoProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedido_produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('localizador')->unique();
+            $table->decimal('desconto', 6, 2)->default(0);
+            $table->enum('modo_desconto', ['valor', 'porc'])->default('porc');
+            $table->decimal('limite', 6, 2)->default(0);
+            $table->enum('modo_limite', ['valor', 'qtd'])->default('qtd');
+            $table->dateTime('dthr_validade');
+            $table->enum('ativo', ['S', 'N'])->default('S');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedido_produtos');
+    }
+}
